dfa8079ae8f5bba83bcfc303cef99daa5bb341db
Hemanth Raj <raj84325@gmail.com>
2017-09-10 15:10:58 +0530

    Added example

---
+++ example/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_3d_obj/flutter_3d_obj.dart';
+
+void main() {
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Flutter 3D Demo',
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: const Text("Flutter 3D"),
+        ),
+        body:new Center(
+              child: new Object_3D(size: const Size(400.0, 400.0),path: "assets/file.obj",asset:true),
+            ),
+      ),
+    );
+  }
+}
+
